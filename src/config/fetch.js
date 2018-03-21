e83ddad8c9e5bc91b9a1e97fac241bdae62d5352
poland5 <sai5585wo@163.com>
2018-03-21 17:46:49 +0800

    fetch注释

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -2,6 +2,8 @@ import { baseUrl } from './env'
 
 export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
 	
+	//根据type类型获取URL地址
+	//http://localhost:8002/v1/users/list?offset=0&limit=20
 	type = type.toUpperCase();
 	url = baseUrl + url;
 
@@ -30,14 +32,18 @@ export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
 			mode: "cors",
 			cache: "force-cache"
 		}
-
 		if (type == 'POST') {
 			Object.defineProperty(requestConfig, 'body', {
 				value: JSON.stringify(data)
 			})
 		}
 
+		//发送数据
 		try {
+
+			//第一个参数是设置请求方法（如post、put或del），Fetch会自动设置方法为get。
+			//第二个参数是设置头部。因为一般使用JSON数据格式，所以设置ContentType为application/json。
+			//第三个参数是设置包含JSON内容的主体。因为JSON内容是必须的，所以当设置主体时会调用JSON.stringify。
 			const response = await fetch(url, requestConfig);
 			const responseJson = await response.json();
 			return responseJson
